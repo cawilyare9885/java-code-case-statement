d822629f62b31f7a25fae336342cb3b17dea2bcd
AWIL <cawilyare9885@gmail.com>
2020-10-27 10:05:03 +0300

    assignment commit 10-27

---
+++ src/test/DoWhileloop_assignment.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author Awil
+ */
+public class DoWhileloop_assignment {
+ public static void main(String args[]){
+    
+     int i = 0;
+do {
+  System.out.println(i);
+  i++;
+}
+while (i < 6);
+
+     
+}
+}
